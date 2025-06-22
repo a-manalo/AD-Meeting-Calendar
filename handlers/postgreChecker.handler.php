c0bacf4ff5f319a4cca007aab9d385f525efeeca
a-manalo <agmnlo.by@gmail.com>
2025-06-22 15:01:06 +0800

    refactor(postgreChecker.handler.php): changed dbname to updated dbname

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = "host.docker.internal"; 
+$host = "host.docker.internal";
 $port = "5556";
 $username = "user";
 $password = "password";
-$dbname = "mydatabase";
+$dbname = "meetcalendardb";
 
 $conn_string = "host=$host port=$port dbname=$dbname user=$username password=$password";
 
