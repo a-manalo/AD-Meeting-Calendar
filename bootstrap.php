8ad837afcbac9f098b844e9dacd3b194b4b724bc
a-manalo <agmnlo.by@gmail.com>
2025-07-08 23:54:19 +0800

    feat(bootstrap.php): define TEMPLATES_PATH and LAYOUTS_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,8 @@
 define('BASE_PATH', realpath(__DIR__));
 define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
 define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('TEMPLATES_PATH', realpath(BASE_PATH . "/components/templates"));
+define('LAYOUTS_PATH', realpath(BASE_PATH . "/layouts"));
 define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
 
 chdir(BASE_PATH);
