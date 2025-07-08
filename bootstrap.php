224e2719fd3b998e8bf73cc02bc9e211b6f1c94b
a-manalo <agmnlo.by@gmail.com>
2025-07-08 16:42:37 +0800

    feat(bootstrap.php) added DUMMIES_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
-define('HANDLERS_PATH', BASE_PATH . '/handlers');
-define('UTILS_PATH', BASE_PATH . '/utils');
+define('HANDLERS_PATH', realpath(BASE_PATH . "/handlers"));
+define('UTILS_PATH', realpath(BASE_PATH . "/utils"));
+define('DUMMIES_PATH', realpath(BASE_PATH . "/staticDatas/dummies"));
 
 chdir(BASE_PATH);
